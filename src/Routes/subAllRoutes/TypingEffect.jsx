d7bd3a3783bd052ece6301352fc00fb676042e28
Harsh Patle <harshpatle491@gmail.com>
2024-11-05 19:14:47 +0530

    updated

--- src/Routes/subAllRoutes/TypingEffect.jsx
+++ src/Routes/subAllRoutes/TypingEffect.jsx
@@ -2,49 +2,46 @@ import { useEffect, useState } from "react"
 import './allroutes.css'
 
 
-export const SetTypingEffect = () =>{
+export const SetTypingEffect = () => {
 
     const data = [
         "Hi! I’m Harsh Patle 👋,",
-        "A developer who turns ideas into reality 💡.",
-        "I craft responsive web applications with React ⚛️.",
-        "My skills include JavaScript, React, and full-stack development 💻.",
-        "Let’s create something amazing together! 🚀",
-        "Feel free to check out my projects and connect with me! 📫"
+        "A developer skilled in JavaScript, React, and full-stack development 💻.",
+        "I build responsive web applications and teach students through college sessions 📚.",
+        "Let’s connect and create something amazing! 🚀"
+
+    ];
+    const [displayText, setDisplayText] = useState("");
+    const [lineIndex, setLineIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
 
 
-    ];  
-     const [displayText, setDisplayText] = useState("");
-   const [lineIndex, setLineIndex] = useState(0);
-   const [charIndex, setCharIndex] = useState(0);
+    useEffect(() => {
 
+        if (lineIndex < data.length) {
+            if (charIndex < data[lineIndex].length) {
+                const timeout = setTimeout(() => {
+                    setDisplayText((prev) => prev + data[lineIndex][charIndex]);
+                    setCharIndex((prev) => prev + 1);
+                }, 200)
+                return () => clearTimeout(timeout);
+            } else {
 
-   useEffect(() =>{
-
-    if(lineIndex < data.length){
-        if(charIndex < data[lineIndex].length){
-            const timeout = setTimeout(() =>{
-                setDisplayText((prev) => prev + data[lineIndex][charIndex]);
-                setCharIndex((prev) => prev + 1);
-            },200)
-            return () => clearTimeout(timeout);
-        }else{
-
-            const lineTimeOut = setTimeout(() =>{
-                setDisplayText((prev) => prev + "\n");
-                setLineIndex((prev) => prev+1);
-                setCharIndex(0);
-            },500)
+                const lineTimeOut = setTimeout(() => {
+                    setDisplayText((prev) => prev + "\n");
+                    setLineIndex((prev) => prev + 1);
+                    setCharIndex(0);
+                }, 500)
+            }
         }
-    }
-   }, [charIndex,lineIndex,data]);
-   
-   
-    return(
+    }, [charIndex, lineIndex, data]);
+
+
+    return (
         <>
-        <div className="typing-Effect">
-            <h1>{displayText}</h1>
-        </div>
+            <div className="typing-Effect">
+                <h1>{displayText}</h1>
+            </div>
 
         </>
     )
