d190d68f9933c16b0baa753c7fbd6cdb8b3134af
Harsh Patle <harshpatle491@gmail.com>
2024-11-11 22:43:49 +0530

    ok

--- src/Routes/subAllRoutes/About.jsx
+++ src/Routes/subAllRoutes/About.jsx
@@ -3,6 +3,11 @@ export const About = () =>{
     return(
         <>
         <h1>About Paojnnjge</h1>
+        <h2>
+            <h3>
+                <h4></h4>
+            </h3>
+        </h2>
         </>
     )
 }
